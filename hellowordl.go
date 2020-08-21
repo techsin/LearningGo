f03bc2165d05693dd74a847b7aff92cdb7ee422e
Muhammad Umer <myemailum14@gmail.com>
2020-08-21 14:03:26 -0400

    Update and rename hellowordl.go to helloworld.go

--- hellowordl.go
+++
@@ -1,5 +0,0 @@
-package main //package name declaration
-
-import "fmt"
-
-func main() {
---
+++ helloworld.go
@@ -0,0 +1,9 @@
+package main //package name declaration
+
+import "fmt"
+
+func main() {
+  fmt.PrintLn('Hello');
+}
+
+// godoc package fn -> provides details about the package/function
